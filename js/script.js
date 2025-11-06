4c5c3e55cd67e3795cfce4848cefdbda53ad4df0
Edward Anderson <edward.a93@hotmail.co.uk>
2025-11-06 04:54:27 +0100

    I´m dom with this DOM exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+const addBtn = document.getElementById("agregar");
+const list = document.getElementById("lista");
+
+addBtn.addEventListener("click", function() {
+  const item = prompt("Type your text here:");
+  console.log(item);
+
+
+
+if (item) {
+    const li = document.createElement("li");
+    li.textContent = item;
+    list.appendChild(li);
+  }
+});
